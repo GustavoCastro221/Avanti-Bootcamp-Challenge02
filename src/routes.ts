a4ab9668ce771948bb90cde34f647b81053d1adf
gus <gustavofring221b@gmail.com>
2023-09-05 08:59:17 -0300

    Added all player routes to be implemented

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,16 @@ const router = Router();
 const createPlayer = new CreatePlayerController();
 const createTeam = new CreateTeamController();
 
+// Rotas de Jogadores
+//router.get("/players", findAllPlayers.handle)
+//router.get("/player/:id", findOnePlayer.handle)
 router.post("/player", createPlayer.handle)
+//router.put("/player/:id", updatePlayer.handle)
+//router.delete("/player/:id", deletePlayer.handle)
+
+//Rotas de Times
 router.post("/team", createTeam.handle)
+
+//Rotas de Campeonatos
 
 export { router };
