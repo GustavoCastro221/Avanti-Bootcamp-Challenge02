b519d00b0856f38c56cadee881e8126dd653ddbc
gus <gustavofring221b@gmail.com>
2023-09-06 08:03:25 -0300

    Implemented FindOnePlayerController and its use

---
+++ src/controllers/FindOnePlayerController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from 'express'
+import {prismaClient} from "../database/prismaClient"
+
+export class FindOnePlayerController{
+    async handle(req: Request, res: Response){
+        try {
+            const { id } = req.params;
+            const player = await prismaClient.player.findFirstOrThrow({
+                where:{
+                    id,
+                }
+        });
+            return res.status(201).json(player);
+        } catch (error) {
+            return res.status(500).json({error: 'An error ocurred when retriving a player'})
+        }
+        
+
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import { FindOnePlayerController } from "./controllers/FindOnePlayerController";
 import { CreatePlayerController } from "./controllers/CreatePlayerController";
 import { CreateTeamController } from "./controllers/CreateTeamController";
 const router = Router();
 const createPlayer = new CreatePlayerController();
 const createTeam = new CreateTeamController();
+const findOnePlayer = new FindOnePlayerController();
 
 // Rotas de Jogadores
 //router.get("/players", findAllPlayers.handle)
-//router.get("/player/:id", findOnePlayer.handle)
+router.get("/player/:id", findOnePlayer.handle)
 router.post("/player", createPlayer.handle)
 //router.put("/player/:id", updatePlayer.handle)
 //router.delete("/player/:id", deletePlayer.handle)
